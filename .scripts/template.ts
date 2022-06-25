c0da61149cc26875d8a097ec529fc21df3d6b4f3
Yehuda Katz <wycats@gmail.com>
2022-06-24 18:17:31 -0700

    Improve templating

--- .scripts/template.ts
+++ .scripts/template.ts
@@ -5,9 +5,10 @@ import type { StarbeamCommandOptions } from "./commands.js";
 import shell from "shelljs";
 import sh from "shell-escape-tag";
 import { readFileSync, write, writeFileSync } from "fs";
-import { packages } from "./packages.js";
+import { getPackages, packages, type Package } from "./packages.js";
 import * as jsonc from "jsonc-parser";
 import { format } from "prettier";
+import chalk from "chalk";
 
 export function TemplateCommand({ root }: StarbeamCommandOptions): Command {
   const TEMPLATES = {
@@ -17,47 +18,34 @@ export function TemplateCommand({ root }: StarbeamCommandOptions): Command {
   return program
     .createCommand("template")
     .description("template a package")
-    .argument("<dir>", "the directory of the package")
+    .option("-p, --package <package-name>", "the package to test", "all")
     .option(
-      "-f, --file <file>",
-      "instead of templating all files, pick a specific file",
-      undefined
+      "-s, --scope <package-scope>",
+      "the scope of the package",
+      "starbeam"
     )
-    .option(
-      "-t, --type <type>",
-      "the type of package (demos, packages, framework)",
-      "packages"
-    )
-    .action((dir, { file, type }) => {
-      const dirs =
-        dir === "all"
-          ? packages(root).map((p) => p.root)
-          : [resolve(root, type, dir)];
+    .action(({ package: packageName, scope }) => {
+      const packages = getPackages(root, packageName, scope);
 
-      for (const dir of dirs) {
-        updatePackageJSON({ root, dir, type });
-        updateTsconfig({ root, dir });
+      for (const pkg of packages) {
+        console.log(chalk.magenta(`=== Updating ${pkg.name} ===`));
+        updatePackageJSON(root, pkg);
+
+        if (pkg.isTypescript) {
+          updateTsconfig(root, pkg);
+        }
       }
     });
 }
 
-function updatePackageJSON({
-  root,
-  dir,
-  type,
-}: {
-  root: string;
-  type: string;
-  dir: string;
-}) {
-  console.log(`Updating ${dir}`);
-  const absoluteDir = resolve(root, type, dir);
+function updatePackageJSON(root: string, pkg: Package) {
+  console.log(chalk.gray(`+ package.json`));
 
   const splice = JSON.parse(
     readFileSync(resolve(root, ".templates", "package", "package.json"), "utf8")
   );
 
-  const editingJSON = resolve(absoluteDir, "package.json");
+  const editingJSON = resolve(pkg.root, "package.json");
 
   const json = JSON.parse(readFileSync(editingJSON, "utf-8"));
 
@@ -66,28 +54,102 @@ function updatePackageJSON({
   writeFileSync(editingJSON, JSON.stringify(json, null, 2));
 }
 
-function updateTsconfig({ root, dir }: { root: string; dir: string }) {
-  const tsconfigFile = resolve(dir, "tsconfig.json");
-  const tsconfig = readFileSync(tsconfigFile, "utf8");
+function updateTsconfig(root: string, pkg: Package) {
+  console.log(chalk.gray(`+ tsconfig.json`));
 
-  const tsconfigJSON = jsonc.parse(tsconfig);
+  const tsconfigFile = resolve(pkg.root, "tsconfig.json");
 
-  const edits = jsonc.modify(
-    tsconfig,
-    ["compilerOptions", "types", typeIndex(tsconfigJSON)],
-    join(relative(dir, resolve(root, "packages")), "env"),
-    {
-      formattingOptions: {
-        tabSize: 2,
-        insertSpaces: true,
-        insertFinalNewline: true,
-      },
-    }
+  const editor = EditJsonc.parse(tsconfigFile);
+
+  editor.addUnique(
+    "compilerOptions.types",
+    "./env",
+    (type) => typeof type === "string" && type.endsWith("/env")
   );
 
-  const output = jsonc.applyEdits(tsconfig, edits);
+  editor.set(
+    "extends",
+    join(relative(pkg.root, resolve(root, "tsconfig.package.json"))),
+    { position: 0 }
+  );
 
-  writeFileSync(tsconfigFile, format(output, { parser: "json" }));
+  editor.write();
+}
+
+class EditJsonc {
+  static parse(filename: string) {
+    try {
+      const source = readFileSync(filename, "utf8");
+      return new EditJsonc(filename, source, jsonc.parse(source));
+    } catch {
+      return new EditJsonc(filename, "{}", jsonc.parseTree("{}")!);
+    }
+  }
+
+  #filename: string;
+  #source: string;
+  #json: jsonc.Node;
+
+  constructor(filename: string, source: string, json: jsonc.Node) {
+    this.#filename = filename;
+    this.#source = source;
+    this.#json = json;
+  }
+
+  addUnique(path: string, value: unknown, check: (json: unknown) => boolean) {
+    if (check(this.#json)) {
+      return;
+    }
+
+    jsonc.findNodeAtLocation;
+
+    const jsonPath = this.#path(path);
+    const node = jsonc.findNodeAtLocation(this.#json, jsonPath);
+
+    if (node && node.type === "array") {
+      const value = node.value as unknown[];
+      const index = value.findIndex((v) => check(v));
+
+      const edit = jsonc.modify(this.#source, [...jsonPath, index], value, {});
+      this.#source = jsonc.applyEdits(this.#source, edit);
+      this.#json = jsonc.parseTree(this.#source)!;
+    } else {
+      const edit = jsonc.modify(this.#source, jsonPath, [value], {});
+      this.#source = jsonc.applyEdits(this.#source, edit);
+      this.#json = jsonc.parseTree(this.#source)!;
+    }
+  }
+
+  set(
+    path: string,
+    value: unknown,
+    {
+      position = -1,
+    }: { position?: number | ((siblings: string[]) => number) } = {}
+  ) {
+    const edit = jsonc.modify(this.#source, this.#path(path), value, {
+      getInsertionIndex:
+        typeof position === "function" ? position : () => position,
+    });
+
+    this.#source = jsonc.applyEdits(this.#source, edit);
+    this.#json = jsonc.parseTree(this.#source)!;
+  }
+
+  write() {
+    const formatted = format(this.#source, { parser: "json" });
+    writeFileSync(this.#filename, formatted);
+  }
+
+  #path(source: string) {
+    return source.split(".").map((p) => {
+      if (/^\d+$/.test(p)) {
+        return Number(p);
+      } else {
+        return p;
+      }
+    });
+  }
 }
 
 function typeIndex(tsconfig: unknown): number {
