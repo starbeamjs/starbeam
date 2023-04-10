af86b0823a35953f9c20be515624ef3ae2f9f9aa
Yehuda Katz <wycats@gmail.com>
2023-04-10 00:16:39 -0700

    Finish useStrictLifecycle Cleanup

--- packages/react/use-strict-lifecycle/index.ts
+++ packages/react/use-strict-lifecycle/index.ts
@@ -1,3 +1,5 @@
+export type { RegisterLifecycleHandlers } from "./src/lifecycle.js";
+export { useLifecycle } from "./src/lifecycle.js";
 export { isRestrictedRead as isRendering } from "./src/react.js";
 export {
   beginReadonly as maskRendering,
@@ -5,10 +7,4 @@ export {
   endReadonly as unmaskRendering,
   unsafeTrackedElsewhere,
 } from "./src/react.js";
-export type { RegisterLifecycleHandlers } from "./src/resource.js";
-export { useLifecycle } from "./src/resource.js";
-export {
-  type Ref,
-  useUpdatingRef,
-  useUpdatingVariable,
-} from "./src/updating-ref.js";
+export { type Ref, useLastRenderRef } from "./src/updating-ref.js";
