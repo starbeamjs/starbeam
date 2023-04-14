82791fadc3acb4085f2dbb236754dfc15b1a2b00
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-04-14 12:33:45 -0400

    Lint: address issues with TS due to changing to require

--- packages/universal/debug/src/setup.ts
+++ packages/universal/debug/src/setup.ts
@@ -5,7 +5,7 @@ if (
   try {
     // this is for CJS only, so require is the only option here
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const buffer = require("buffer");
+    const buffer = require("buffer") as { Buffer: BufferConstructor };
     globalThis.Buffer = buffer.Buffer;
   } catch {
     // ignore
