61a79f18d854698de4f1ce16b3cad8a5f150e98c
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-04-14 12:29:38 -0400

    Lint: fix @starbeam/debug's setup file, because CJS can't use top-level await

--- packages/universal/debug/src/setup.ts
+++ packages/universal/debug/src/setup.ts
@@ -3,6 +3,8 @@ if (
   typeof require === "function"
 ) {
   try {
+    // this is for CJS only, so require is the only option here
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
     const buffer = require("buffer");
     globalThis.Buffer = buffer.Buffer;
   } catch {
