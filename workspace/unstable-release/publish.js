19ee3e39808d36b5f6643d57c08b31c39088952a
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-06-24 22:00:12 -0400

    Add new package and workflow for publishing unstable releases

---
+++ workspace/unstable-release/publish.js
@@ -0,0 +1,34 @@
+import { execaCommand } from "execa";
+import { listPublicWorkspaces } from "./workspaces.js";
+import { dirname } from "path";
+
+async function publish() {
+  let publicWorkspaces = await listPublicWorkspaces();
+
+  const errors = [];
+
+  for (let workspace of publicWorkspaces) {
+    console.info(`Publishing ${workspace}`);
+    try {
+      await execaCommand("npm publish --tag=unstable --verbose", {
+        cwd: dirname(workspace),
+      });
+    } catch (err) {
+      console.info(
+        `Publishing ${workspace} has failed. A full list of errors will be printed at the end of this run`
+      );
+      errors.push(err);
+      continue;
+    }
+
+    console.info(`Publishing ${workspace} completed successfully!`);
+  }
+
+  if (errors.length) {
+    console.error("Errors were encountered while publishing these packages");
+    errors.forEach((error) => console.log(error.stderr));
+    process.exit(1);
+  }
+}
+
+publish();
---
+++ workspace/unstable-release/workspaces.js
@@ -0,0 +1,30 @@
+import globby from "globby";
+import { execaCommand } from "execa";
+import fse from "fs-extra";
+
+/**
+ * All publishable packages are in packages/*
+ *
+ * We could read package.json#workspaces, but then we'd have more to filter out.
+ */
+export async function listPublicWorkspaces() {
+  let filePaths = await globby(["packages/*/package.json"]);
+
+  let result = [];
+
+  for (let filePath of filePaths) {
+    let packageJson = await fse.readJSON(filePath);
+
+    if (packageJson.private) continue;
+
+    result.push(filePath);
+  }
+
+  return result;
+}
+
+export async function currentSHA() {
+  let { stdout } = await execaCommand(`git rev-parse --short HEAD`);
+
+  return stdout.trim();
+}
