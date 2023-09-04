75b44471fc9c861d1118b21bd8974ad1f1243317
Yehuda Katz <wycats@gmail.com>
2023-09-04 14:19:36 -0700

    Add getServiceFormula
    getServiceFormula is a low-level API that renderers can use to
    synchronize all services together with the root component (or
    equivalent).

--- packages/universal/service/src/service.ts
+++ packages/universal/service/src/service.ts
@@ -1,10 +1,10 @@
 import type { Description } from "@starbeam/interfaces";
-import { DEBUG } from "@starbeam/reactive";
-import type { IntoResourceBlueprint } from "@starbeam/resource";
-import { Resource, type ResourceBlueprint, setup } from "@starbeam/resource";
+import { Marker } from "@starbeam/reactive";
+import { CachedFormula, DEBUG, type FormulaFn } from "@starbeam/reactive";
+import type { SyncFn } from "@starbeam/resource";
+import { Resource, type ResourceBlueprint } from "@starbeam/resource";
 import { CONTEXT } from "@starbeam/runtime";
-
-type Blueprint<T> = IntoResourceBlueprint<T>;
+import { mountFinalizationScope } from "@starbeam/shared";
 
 /**
  * The `Service` function takes a resource blueprint and turns it into a
@@ -12,38 +12,91 @@ type Blueprint<T> = IntoResourceBlueprint<T>;
  * components rendered within a given app instance.s
  */
 export function Service<T>(
-  blueprint: Blueprint<T>,
+  blueprint: ResourceBlueprint<T>,
   {
     description,
     app = CONTEXT.app,
-  }: { description?: string | Description | undefined; app?: object } = {}
+  }: { description?: string | Description | undefined; app?: object } = {},
 ): ResourceBlueprint<T> {
-  return Resource(({ use }) => {
-    return CONTEXT.create(
-      blueprint,
-      () => {
-        return use(blueprint);
-      },
-      { app }
-    );
-  }, DEBUG?.Desc("service", description));
+  return Resource(
+    ({ use }) => {
+      return CONTEXT.create(
+        blueprint,
+        () => {
+          return use(blueprint);
+        },
+        { app },
+      );
+    },
+    DEBUG?.Desc("service", description),
+  );
 }
 
-export function service<T>(
-  resource: Blueprint<T>,
+interface ServiceState {
+  readonly marker: Marker;
+  readonly syncs: Set<SyncFn<void>>;
+  readonly formula: FormulaFn<void>;
+}
+
+const SERVICES = new WeakMap<object, ServiceState>();
+
+function initializeServices(app: object) {
+  let services = SERVICES.get(app);
+
+  if (!services) {
+    const marker = Marker();
+    const syncs = new Set<SyncFn<void>>();
+
+    const formula = CachedFormula(() => {
+      marker.read();
+      for (const sync of syncs) sync();
+    });
+
+    services = { marker, syncs, formula };
+    SERVICES.set(app, services);
+  }
+
+  return services;
+}
+
+function addService(app: object, service: SyncFn<void>) {
+  const { syncs, marker } = initializeServices(app);
+  syncs.add(service);
+  marker.mark();
+}
+
+/**
+ *
+ * @category primitive
+ */
+export function getServiceFormula(app: object): FormulaFn<void> {
+  return initializeServices(app).formula;
+}
+
+export function service<const T>(
+  resource: ResourceBlueprint<T>,
   {
-    description,
+    description: _description,
     app = CONTEXT.app,
   }: {
     description?: string | Description | undefined;
     app?: object | undefined;
-  } = {}
-): Resource<T> {
-  return setup(
-    Service(resource, {
-      description: DEBUG?.Desc("service", description),
-      app,
-    }),
-    { lifetime: app }
+  } = {},
+): T {
+  return CONTEXT.create(
+    resource,
+    () => {
+      const done = mountFinalizationScope(app);
+      try {
+        const { sync, value } = resource.setup();
+
+        addService(app, sync);
+
+        return value;
+      } finally {
+        done();
+      }
+    },
+    { app },
   );
 }
