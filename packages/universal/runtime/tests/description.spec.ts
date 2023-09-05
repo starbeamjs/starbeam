3eb6b2e74b7d48f31f67a28fc09f0d12cfd58d7c
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-09-05 18:11:06 -0400

    get ci green?

--- packages/universal/runtime/tests/description.spec.ts
+++ packages/universal/runtime/tests/description.spec.ts
@@ -1,16 +1,21 @@
-import { expect, test } from "@starbeam-workspace/test-utils";
 import { DEBUG } from "@starbeam/debug";
+import { expect, test } from "@starbeam-workspace/test-utils";
 
 test("inferred api", () => {
-  expect(SomeAPI()?.api).toMatchObject({
-    type: "simple",
-    name: "SomeAPI",
-  });
+  if (import.meta.env.DEV) {
+    expect(SomeAPI()?.api).toMatchObject({
+      type: "simple",
+      name: "SomeAPI",
+    });
 
-  expect(ArrowFn()?.api).toMatchObject({
-    type: "simple",
-    name: "ArrowFn",
-  });
+    expect(ArrowFn()?.api).toMatchObject({
+      type: "simple",
+      name: "ArrowFn",
+    });
+  } else {
+    expect(SomeAPI()?.api).toBe(undefined);
+    expect(ArrowFn()?.api).toBe(undefined);
+  }
 });
 
 function SomeAPI() {
