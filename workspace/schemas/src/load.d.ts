bd65d6000cfcc6ccfa7b017bc2e5e80f8803e9ff
Yehuda Katz <wycats@gmail.com>
2023-09-23 00:23:57 -0700

    Overhaul the infrastructure for defining scripts

--- workspace/schemas/src/load.d.ts
+++ workspace/schemas/src/load.d.ts
@@ -1,3 +1,2 @@
 export function loadJSON<T>(file: string, schema: string): T;
-export function loadToml<T>(file: string, schema: string): T;
 export function validate<T>(object: object, schema: string): T;
