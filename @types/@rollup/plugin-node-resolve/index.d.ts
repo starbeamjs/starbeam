9b57609fad8e0c4619e51d1da5e4612e389dee47
Yehuda Katz <wycats@gmail.com>
2023-10-12 20:49:40 -0700

    Remove extracted packages

--- @types/@rollup/plugin-node-resolve/index.d.ts
+++
@@ -1,5 +0,0 @@
-import type Plugin from "@rollup/plugin-node-resolve";
-
-declare const plugin: () => Plugin;
-
-export default plugin;
--- workspace/compile/index.js
+++
@@ -1,3 +0,0 @@
-export { default as importMeta } from "./src/rollup/plugins/import-meta.js";
-export { default as inline } from "./src/rollup/plugins/inline.js";
-export { compile } from "./src/rollup/rollup.js";
