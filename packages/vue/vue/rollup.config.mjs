dfeb9be3ef9f59026a524fed6f2f5d2dd5cf0c80
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-01-04 14:30:51 -0500

    Use the changelog from the monorepo root in each package

--- packages/vue/vue/rollup.config.mjs
+++ packages/vue/vue/rollup.config.mjs
@@ -1,3 +1,27 @@
-import { compile } from "@starbeam-dev/compile";
 
-export default compile(import.meta);
+import { execSync } from 'node:child_process';
+import path from 'node:path';
+
+import { compile } from "@starbeam-dev/compile";
+import copy from 'rollup-plugin-copy'
+
+const config = compile(import.meta);
+
+const monorepoRoot = execSync('git rev-parse --show-toplevel', { encoding: 'utf-8' }).trim();
+
+const rootChangelog = path.join(monorepoRoot, 'CHANGELOG.md');
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment
+const includeChangelog = copy({ 
+  targets: [
+    {
+      src: rootChangelog,
+      dest: 'CHANGELOG.md',
+    }
+  ],
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+config[0].plugins.push(includeChangelog);
+
+export default config;
