5ee8282cc4f157c7b9bea0d7b1d4eca2ab9a3f11
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-01-04 15:40:33 -0500

    Revert local config / package changes

--- packages/universal/collections/rollup.config.mjs
+++ packages/universal/collections/rollup.config.mjs
@@ -1,27 +1,3 @@
-
-import { execSync } from 'node:child_process';
-import path from 'node:path';
-
 import { compile } from "@starbeam-dev/compile";
-import copy from 'rollup-plugin-copy'
 
-const config = compile(import.meta);
-
-const monorepoRoot = execSync('git rev-parse --show-toplevel', { encoding: 'utf-8' }).trim();
-
-const rootChangelog = path.join(monorepoRoot, 'CHANGELOG.md');
-
- 
-const includeChangelog = copy({ 
-  targets: [
-    {
-      src: rootChangelog,
-      dest: '.',
-    }
-  ],
-});
-
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-config[0].plugins.push(includeChangelog);
-
-export default config;
+export default compile(import.meta);
