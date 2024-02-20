d25b598d73de16d543c3ccc82f9ddb45ce28feff
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-02-20 16:38:59 -0500

    no looking

--- packages/universal/core/index.ts
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line no-console
-console.warn(
-  "WARNING: @starbeam/core has been renamed to @starbeam/universal. Please update your imports to refer to @starbeam/universal.",
-);
-export * from "@starbeam/universal";
--- packages/universal/verify/tests/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# @starbeam-tests/verify
-
-### Patch Changes
-
-- Updated dependencies [2a957e5]
-- Updated dependencies [e03c2a0]
-  - @starbeam/verify@0.5.1
