8ba005187329e63a71639a56eae37efdd200da59
Yehuda Katz <wycats@gmail.com>
2026-05-01 14:56:34 -0700

    Make debug an internal package (#184)

--- packages/universal/universal/rollup.config.mjs
+++ packages/universal/universal/rollup.config.mjs
@@ -1,3 +1,9 @@
 import { compile } from "@starbeam-dev/compile";
 
-export default compile(import.meta);
+export default compile(import.meta).map((config) => ({
+  ...config,
+  output: {
+    ...config.output,
+    sourcemapExcludeSources: true,
+  },
+}));
