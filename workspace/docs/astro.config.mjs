7707cf1b09906c3eda769c045fcce303bfcbea53
Yehuda Katz <wycats@gmail.com>
2026-05-16 20:55:15 -0700

    docs: configure Vercel deployment

--- workspace/docs/astro.config.mjs
+++ workspace/docs/astro.config.mjs
@@ -2,6 +2,7 @@ import starlight from "@astrojs/starlight";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://starbeamjs.com",
   devToolbar: {
     enabled: false,
   },
